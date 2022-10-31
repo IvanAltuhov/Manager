2fd2fb230fe6822bf91ecc6adbd0cd78aa6f79a9
IvanAltuhov <altuhov494@mail.ru>
2022-10-31 23:05:23 +0300

    v1 stat

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,20 @@ public class Main {
         int[] sales = {1,12,3,2,232,12,13,777};
         Manager sm = new Manager(sales);
         System.out.println(sm.max());
+        int max = sales[0], min = sales[0];
+        for(int i = 0; i < sales.length; i++) {
+            if(sales[i] > max)
+                max = sales[i];
+
+            if(sales[i] < min)
+                min = sales[i];
+        }
+        int result = 0;
+        for (int m : sales) {
+            result += m;
+        }
+        System.out.println("Значение среднего арифметического равно: " + (result - max - min)/sales.length);
+
     }
 }
 
